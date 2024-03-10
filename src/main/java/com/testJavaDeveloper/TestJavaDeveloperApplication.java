63db8b4006d7fff61b72f667e5a1ca65a43991b4
João Pedro <joaopedrodamaiamoura@outlook.com>
2024-03-10 01:52:23 -0300

    feat: initial configuration

---
+++ src/main/java/com/testJavaDeveloper/TestJavaDeveloperApplication.java
@@ -0,0 +1,13 @@
+package com.testJavaDeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJavaDeveloperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJavaDeveloperApplication.class, args);
+	}
+
+}
